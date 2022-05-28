36aceb3832cad79bae814e6b3888843f2ccd28f7
haoranchen18 <haoran.chen18@gmail.com>
2022-05-27 20:40:30 -0700

    Updates

---
+++ precache-manifest.ec26ba58035f52a0cc5de0e09a89ebfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e659c621e38b0545a63",
+    "url": "/grocery-store-checklist/static/css/main.1f1cbcbb.chunk.css"
+  },
+  {
+    "revision": "2e659c621e38b0545a63",
+    "url": "/grocery-store-checklist/static/js/main.2e659c62.chunk.js"
+  },
+  {
+    "revision": "aaf33d6a2fa40dccd0f7",
+    "url": "/grocery-store-checklist/static/js/1.aaf33d6a.chunk.js"
+  },
+  {
+    "revision": "1fbde81fb5e6ced8a763",
+    "url": "/grocery-store-checklist/static/js/runtime~main.1fbde81f.js"
+  },
+  {
+    "revision": "c5339b1757e53a4c37f56c975e86691a",
+    "url": "/grocery-store-checklist/index.html"
+  }
+];
